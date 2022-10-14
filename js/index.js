cd77ec687769534d61c3c909542750428edfcddc
Facundo Nuñez <fnunez1981@gmail.com>
2022-10-13 22:57:14 -0300

    agregando libreria

--- js/index.js
+++ js/index.js
@@ -3,62 +3,71 @@
 const contenedor = document.getElementById("contenedor");
 const contenedorDos = document.getElementById("contenedorDos");
 const formulario = document.getElementById("formulario");
-//const botonComprar = document.getElementById("botonComprar");
+const vaciar = document.getElementById("vaciar");
 const productosComprados = document.getElementById("productosComprados");
 
-
-const bicicletas = [
-        { id: 1,
+const bicicletas = [{
+        id: 1,
         nombre: "Bicicleta Junior",
         precio: 4500,
-        imagen: "./img/bicicleta-junior.jpg"
-        },
+        imagen: "./img/bicicleta-junior.jpg",
+    },
 
-        { id: 2,
-            nombre: "Bicicleta Urbana",
-            precio: 54500,
-            imagen: "./img/bicicleta-urbana.jpg"
-        },
+    {
+        id: 2,
+        nombre: "Bicicleta Urbana",
+        precio: 54500,
+        imagen: "./img/bicicleta-urbana.jpg",
+    },
 
-        { id: 3,
-            nombre: "Bicicleta Carrera",
-            precio: 144500,
-            imagen: "./img/bicicletasdecarrera_4.jpg"
-        },
+    {
+        id: 3,
+        nombre: "Bicicleta Carrera",
+        precio: 144500,
+        imagen: "./img/bicicletasdecarrera_4.jpg",
+    },
 
-        { id: 4,
-            nombre: "MTB Carbono",
-            precio: 204400,
-            imagen: "./img/carbono.jpg"
-        },
+    {
+        id: 4,
+        nombre: "MTB Carbono",
+        precio: 204400,
+        imagen: "./img/carbono.jpg"
+    },
 
-        { id: 5,
-            nombre: "MTB Feature",
-            precio: 104500,
-            imagen: "./img/FeatureBiciMontana.jpg"
-        },
+    {
+        id: 5,
+        nombre: "MTB Feature",
+        precio: 104500,
+        imagen: "./img/FeatureBiciMontana.jpg",
+    },
 
-        { id: 6,
-            nombre: "Mono Electrico",
-            precio: 94800,
-            imagen: "./img/monopatin.jpg"
-        },
-                        
-        { id: 7,
-            nombre: "Bicicleta Madera",
-            precio: 9650,
-            imagen: "./img/patacleta.jpg"
-        }
+    {
+        id: 6,
+        nombre: "Mono Electrico",
+        precio: 94800,
+        imagen: "./img/monopatin.jpg",
+    },
 
-]
+    {
+        id: 7,
+        nombre: "Bicicleta Madera",
+        precio: 9650,
+        imagen: "./img/patacleta.jpg",
+    },
+];
 
-let carrito = []
+let carrito = [];
 localStorage.setItem("carrito", JSON.stringify(bicicletas));
 
 //render
-bicicletas.forEach(bicicletas => {
-    const {id, nombre, precio, imagen} = bicicletas//desestructuracion de objetos
-    let muestra = document.createElement("div")
+bicicletas.forEach((bicicletas) => {
+    const {
+        id,
+        nombre,
+        precio,
+        imagen
+    } = bicicletas; //desestructuracion de objetos
+    let muestra = document.createElement("div");
     muestra.innerHTML = `
             <div class="card" style="width: 13rem;">
             <img src="${imagen}" class="card-img-top" alt="bicicletas">
@@ -68,23 +77,37 @@ bicicletas.forEach(bicicletas => {
                     <p class="card-text">${id}</p>    
                     <button id="${id}" class="btn btn-dark">Comprar</button>
                 </div>
-            </div>`
+            </div>`;
 
-        contenedor.append(muestra);
-        const boton = document.getElementById(id)
-        boton.addEventListener("click", () => comprar(bicicletas))
+    contenedor.append(muestra);
+    const boton = document.getElementById(id);
+    boton.addEventListener("click", () => {
+        comprar(bicicletas);
+        Swal.fire({
+            title: "Agregaste",
+            text: `${nombre}`,
+            imageUrl: `${imagen}`,
+            imageWidth: 400,
+            imageHeight: 200,
+        });
+    });
 });
 
 //evento
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
-    contenedorDos.innerHTML= "";
+    contenedorDos.innerHTML = "";
     let inputs = e.target.children;
 
     let producto = bicicletas.find((item) => item.nombre === inputs[0].value);
-    const {id, nombre, precio, imagen} = producto//desestructuracion de objetos
+    const {
+        id,
+        nombre,
+        precio,
+        imagen
+    } = producto; //desestructuracion de objetos
     let div = document.createElement("div");
-        div.innerHTML = `
+    div.innerHTML = `
         <div class="card" style="width: 13rem;">
             <img src="${imagen}" class="card-img-top" alt="bicicletas">
                 <div class="card-body">
@@ -93,24 +116,24 @@ formulario.addEventListener("submit", (e) => {
                     <p class="card-text">${id}</p>
                     <button id="${id}" class="btn btn-dark">Comprar</button>
                 </div>
-            </div>`
+            </div>`;
 
     contenedorDos.append(div);
 });
 
 //operador spread
-const comprar = (bicicletas) =>{
-    let bicisComprada = carrito.find(item => item.id === bicicletas.id)
-    if (bicisComprada === undefined){
+const comprar = (bicicletas) => {
+    let bicisComprada = carrito.find((item) => item.id === bicicletas.id);
+    if (bicisComprada === undefined) {
         carrito.push({
             ...bicicletas,
-            cantidad: 1
-        })
-    }else{
+            cantidad: 1,
+        });
+    } else {
         bicisComprada.precio = bicisComprada.precio + bicicletas.precio;
         bicisComprada.cantidad = bicisComprada.cantidad + 1;
     }
-}
+};
 
-productosComprados.addEventListener("click",() => console.log(carrito))
-productosComprados.addEventListener("click",() => localStorage.setItem("carrito", JSON.stringify(carrito)))
+productosComprados.addEventListener("click", () => console.log(carrito));
+productosComprados.addEventListener("click", () => localStorage.setItem("carrito", JSON.stringify(carrito)));
