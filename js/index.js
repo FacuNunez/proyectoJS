0684e5e1978eb6021dab827c7575d75b07a066f3
Facundo Nuñez <fnunez1981@gmail.com>
2022-10-20 22:14:04 -0300

    fetch II

--- js/index.js
+++ js/index.js
@@ -6,75 +6,26 @@ const formulario = document.getElementById("formulario");
 const vaciar = document.getElementById("vaciar");
 const productosComprados = document.getElementById("productosComprados");
 
-/* const bicicletas = [{
-        id: 1,
-        nombre: "Bicicleta Junior",
-        precio: 4500,
-        imagen: "./img/bicicleta-junior.jpg",
-    },
 
-    {
-        id: 2,
-        nombre: "Bicicleta Urbana",
-        precio: 54500,
-        imagen: "./img/bicicleta-urbana.jpg",
-    },
 
-    {
-        id: 3,
-        nombre: "Bicicleta Carrera",
-        precio: 144500,
-        imagen: "./img/bicicletasdecarrera_4.jpg",
-    },
 
-    {
-        id: 4,
-        nombre: "MTB Carbono",
-        precio: 204400,
-        imagen: "./img/carbono.jpg"
-    },
 
-    {
-        id: 5,
-        nombre: "MTB Feature",
-        precio: 104500,
-        imagen: "./img/FeatureBiciMontana.jpg",
-    },
-
-    {
-        id: 6,
-        nombre: "Mono Electrico",
-        precio: 94800,
-        imagen: "./img/monopatin.jpg",
-    },
-
-    {
-        id: 7,
-        nombre: "Bicicleta Madera",
-        precio: 9650,
-        imagen: "./img/patacleta.jpg",
-    },
-]; */
-
-let carrito = [];
-localStorage.setItem("carrito", JSON.stringify(bicicletas));
-
-//render
-fetch(".//bicicletas.json")
-    .then((response) => response.json())
-    .then((data) => {
-        data.forEach((publicacion) => {
-            let muestra = document.createElement("div");
-            muestra.innerHTML = `
-            <div class="card" style="width: 13rem;">
-            <img src="${publicacion.imagen}" class="card-img-top" alt="bicicletas">
+fetch("./json/bicicletas.json")
+.then(response => response.json())
+.then(data => data.forEach (bicicletas => {
+        const {id, nombre, precio, imagen} = bicicletas
+        let muestra = document.createElement("div")
+        muestra.innerHTML = `
+        <div class="card" style="width: 13rem;">
+            <img src="${imagen}" class="card-img-top" alt="bicicletas">
                 <div class="card-body">
-                    <h3 class="card-title">${publicacion.nombre}</h3>
-                    <h4 class="card-title">$${publicacion.precio}</h4>
-                    <p class="card-text">${publicacion.id}</p>    
-                    <button id="${publicacion.id}" class="btn btn-dark">Comprar</button>
+                    <h3 class="card-title">${nombre}</h3>
+                    <h4 class="card-title">$${precio}</h4>
+                    <p class="card-text">${id}</p>    
+                    <button id="${id}" class="btn btn-dark">Comprar</button>
                 </div>
-            </div>`;
+            </div>`
+        
             contenedor.append(muestra);
             const boton = document.getElementById(id);
             boton.addEventListener("click", () => {
@@ -87,41 +38,10 @@ fetch(".//bicicletas.json")
                     imageHeight: 200,
                 });
             });
-        });
-    })
-    .catch((error) => console.log(error));
-/* bicicletas.forEach((bicicletas) => {
-    const {
-        id,
-        nombre,
-        precio,
-        imagen
-    } = bicicletas; //desestructuracion de objetos
-    let muestra = document.createElement("div");
-    muestra.innerHTML = `
-            <div class="card" style="width: 13rem;">
-            <img src="${imagen}" class="card-img-top" alt="bicicletas">
-                <div class="card-body">
-                    <h3 class="card-title">${nombre}</h3>
-                    <h4 class="card-title">$${precio}</h4>
-                    <p class="card-text">${id}</p>    
-                    <button id="${id}" class="btn btn-dark">Comprar</button>
-                </div>
-            </div>`;
+}))
 
-    contenedor.append(muestra);
-    const boton = document.getElementById(id);
-    boton.addEventListener("click", () => {
-        comprar(bicicletas);
-        Swal.fire({
-            title: "Agregaste",
-            text: `${nombre}`,
-            imageUrl: `${imagen}`,
-            imageWidth: 400,
-            imageHeight: 200,
-        });
-    });
-}); */
+let carrito = [];
+
 
 //evento
 formulario.addEventListener("submit", (e) => {
@@ -130,7 +50,12 @@ formulario.addEventListener("submit", (e) => {
     let inputs = e.target.children;
 
     let producto = bicicletas.find((item) => item.nombre === inputs[0].value);
-    const { id, nombre, precio, imagen } = producto; //desestructuracion de objetos
+    const {
+        id,
+        nombre,
+        precio,
+        imagen
+    } = producto; //desestructuracion de objetos
     let div = document.createElement("div");
     div.innerHTML = `
         <div class="card" style="width: 13rem;">
@@ -161,6 +86,4 @@ const comprar = (bicicletas) => {
 };
 
 productosComprados.addEventListener("click", () => console.log(carrito));
-productosComprados.addEventListener("click", () =>
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-);
+productosComprados.addEventListener("click", () => localStorage.setItem("carrito", JSON.stringify(carrito)));
